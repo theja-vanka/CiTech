9636053927df12ecd113a2a0e6a2f2199dd37596
Vanka <theja.vanka@gmail.com>
2016-07-08 12:59:48 +0530

    Html of about

---
+++ app/src/main/java/com/citech/vanka/cambridge/About.java
@@ -0,0 +1,18 @@
+package com.citech.vanka.cambridge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class About extends AppCompatActivity {
+    WebView myWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        myWebView = (WebView)findViewById(R.id.about);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadUrl();
+    }
+
+}
